b4d86910a06d5c6368d1e65069d4489a2d264e43
phamcuong8868 <phamcuong8868@gmail.com>
2025-10-26 22:00:42 +0700

    upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# music-player-html
+# Trình phát nhạc online phiên bản thuần html
+Các list nhạc dành cho Mai Linh. Không bao gồm tính năng điều khiển từ xa
+Version 3.0
+## Cách sử dụng:
+1. Click Chọn các bài hát để tạo thành 1 list các bài sẽ phát. Nếu không chọn tức là sẽ phát tất cả các bài.
+   
+   Việc click chọn sẽ không ảnh hưởng tới bài đang phát hoặc play bài được click
+3. Lựa chọn chế độ phát:
+   - Thứ tự phát:
+
+     +) Tuần tự theo thứ tự bài đã chọn
+
+     +) Phát xáo trộn
+
+    - Lặp lại:
+
+      +) Lặp lại 1 bài
+
+      +) Lặp lại tất cả các bài đã chọn
+   - Bấm nút control để phát nhạc
+4. Chức năng hẹn giờ:
+
+- Bấm vào icon đồng hồ
+
+- Chọn chế độ hẹn giờ:
+
++) Đếm ngược: số giờ, phút
+
++) Mốc thời gian dừng: đến hh:mm  - đã ok
+
---
+++ cds/listCD.js
@@ -0,0 +1,46 @@
+function cloneBaiHat(bh) 
+{
+    return new BaiHat(JSON.parse(JSON.stringify(bh)));
+}
+
+var allCD = [];
+
+var cdNhacNen = [];
+var cdChaoCo = []; //bao gồm nhạc lễ và nhạc nền
+var cdNhacTruongLopEm = [];
+
+
+///////////////////////////////////////////////
+for (let i = 0; i < listTruongLop.length; i++)
+{
+    cdNhacTruongLopEm.push(cloneBaiHat(listTruongLop[i]));
+}
+
+for (let i = 0; i < cdNhacTruongLopEm.length; i++) 
+{
+    cdNhacTruongLopEm[i].trackId = i;
+    cdNhacTruongLopEm[i].mediaId = `music_${i}`;
+}
+
+
+
+
+////////////////////////////////////////////////
+listNhacLe.forEach(e =>
+{
+    cdChaoCo.push(cloneBaiHat(e));
+});
+
+listNhacNen.forEach(e=>
+{
+    cdChaoCo.push(cloneBaiHat(e));
+});
+
+for (let i = 0; i < cdChaoCo.length; i++) 
+{
+    cdChaoCo[i].trackId = i;
+    cdChaoCo[i].mediaId = `music_${i}`;
+}
+
+allCD.push(cdChaoCo);
+allCD.push(cdNhacTruongLopEm);
